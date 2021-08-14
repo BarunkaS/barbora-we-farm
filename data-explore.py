53b97f7680185f39c62ac89a90d270449be5ec6d
BarunkaS <34685093+BarunkaS@users.noreply.github.com>
2021-08-14 12:10:22 +0100

    Explore data values
    Do initial explore with info, head, shape.
    
    Explore unique values of products, statuses.

--- data-explore.py
+++ data-explore.py
@@ -12,3 +12,14 @@ with gzip.open(file_path, "r") as file:
    json_voucher = json.loads(voucher_file)
    voucher_rows = list(json_voucher.values())[0]
    voucher_df = pd.DataFrame(voucher_rows)
+
+# Explore the df
+print(voucher_df.info())
+print(voucher_df.shape)
+print(voucher_df.head())
+
+
+
+print(voucher_df['product'].unique())
+print(type(voucher_df['vendor']))
+print(voucher_df['status'].unique())
