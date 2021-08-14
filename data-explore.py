258b0bc5dfa6db85b68b98a1eb056e703689eb6c
BarunkaS <34685093+BarunkaS@users.noreply.github.com>
2021-08-14 11:53:47 +0100

    Read path and open single file

--- data-explore.py
+++ data-explore.py
@@ -2,3 +2,13 @@ import gzip
 import json
 import pandas as pd
 
+# Read file path
+file_path = "/Users/barboraspacilova/Documents/DATA/LEARN/Python/WeFarm/" \
+            "barbora-we-farm/data/vouchers/vouchers-2019-11-01.json.gz"
+
+# Open single json to dataframe
+with gzip.open(file_path, "r") as file:
+   voucher_file = file.read()
+   json_voucher = json.loads(voucher_file)
+   voucher_rows = list(json_voucher.values())[0]
+   voucher_df = pd.DataFrame(voucher_rows)
